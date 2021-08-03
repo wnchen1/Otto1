d084c37e18a7c8590ec8f8dbc93f6c42baa22b02
kimsmither <kim.smither@georgebrown.ca>
2021-08-02 20:48:41 -0400

    collision commented out

--- Project/Player.h
+++ Project/Player.h
@@ -12,17 +12,12 @@ public:
 	// Life Cycle Methods
 	virtual void Render() override;
 	virtual void Update(float deltaTime) override;
-	void Stop() { speed = 0; }
-	void Resume() { speed = .5; }
-	void SetX(float x) { m_destinationTransform.x = x; }
-	void SetY(float y) { m_destinationTransform.y = y; }
 
 private:
 	enum class PlayerState { kIdle, kRunning, kAttacking };
 	bool m_facingLeft;
-	int speed = 0.5;
+	double speed = 0.5;
 	PlayerState m_state;
-	/*void m_buildAnimations();*/
 };
 
 #endif /* defined (__PLAYER__) */s
